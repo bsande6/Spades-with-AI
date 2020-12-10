a0151b29a738ba1d046eaa6c9a541f52085cc90c
Brad Sanders <bsande6@g.clemson.edu>
2020-12-10 14:10:30 -0500

    Created gamescreen class

---
+++ gamescreen.js
@@ -0,0 +1,37 @@
+class GameScreen {
+    constructor() {
+        this.canvas = document.querySelector('canvas')
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
+        this.ctx = this.canvas.getContext("2d");
+        this.ctx.fillRect(100, 100, 100, 100);
+
+        var mouse = {
+            x: undefined,
+            y: undefined
+        };
+
+        this.rect = {
+            x: 100,
+            y: 100,
+            width: 100,
+            height: 100
+        };
+    }
+
+    getMousePos(evt) {
+        var rect = this.canvas.getBoundingClientRect();
+        return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+        };
+    }
+
+    isInside(pos){
+        console.log(pos)
+        console.log(pos.x)
+        console.log(this.rect.x)
+        return pos.x > this.rect.x && pos.x < this.rect.x+this.rect.width && pos.y < this.rect.y+
+        this.rect.height && pos.y > this.rect.y
+    }
+}
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 NUM_PLAYERS = 4;
 
-main = function(canvas) {
+main = function() {
+    var screen = new GameScreen();
     var playerList = [];
     for (var i = 0; i < NUM_PLAYERS; i++) {
         player = new Player(i + 1);
@@ -8,6 +9,18 @@ main = function(canvas) {
     }
     deck = new Deck();
     deck.shuffle();
+    window.addEventListener('click', function(event) {
+        var mousePos = screen.getMousePos(event);
+        console.log(mousePos)
+        console.log(mousePos.x)
+    
+        if (screen.isInside(mousePos)) {
+           deck.dealCards(playerList)
+        }else{
+            alert('clicked outside rect');
+        }   
+    }, false);
+    
    
 }
 
